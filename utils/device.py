2bed90f80e26e21c4bc1a0279d9c03ad4369b4a8
Igor Zelenin <i@zelamundo.org>
2024-07-11 17:31:05 +0200

    feat: add a device setter function

---
+++ utils/device.py
@@ -0,0 +1,18 @@
+import torch
+
+
+def set_device():
+    """
+    Set the device to MPS or CUDA if available.
+    :return: device, device_count
+    """
+    if torch.backends.mps.is_available():
+        print("MPS device found.")
+        return torch.device("mps"), 0
+    elif torch.cuda.is_available():
+        device_count = torch.cuda.device_count()
+        print(f"CUDA devices found. Device count: {device_count}")
+        return torch.device("cuda"), device_count
+    else:
+        print("No GPU found. Using CPU.")
+        return torch.device("cpu"), 0
